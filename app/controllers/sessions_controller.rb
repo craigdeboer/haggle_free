3fb8c71bc960c7293c417306b440882593e482a8
Craig Deboer <craig@cdeboer.net>
2015-02-25 16:21:55 -0800

    Clean up layout.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
 	def create
 		@user = User.find_by(email: params[:session][:email].downcase)
 		if @user && @user.authenticate(params[:session][:password])
-			flash[:success] = "The user has been found"
 			log_in(@user)
 			redirect_to root_path
 		else

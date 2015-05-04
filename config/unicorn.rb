33003632fee8e082c4ee131ce47b40445eb9fee0
Craig Deboer <craig@cdeboer.net>
2015-05-04 14:46:49 -0700

    Change Unicorn pid location.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ root = "/home/craig/apps/hagglefree/current"
 listen "127.0.0.1:8080"
 worker_processes 2
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "home/craig/apps/hagglefree/shared/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"

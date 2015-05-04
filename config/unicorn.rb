92af7fbaacb4f712cc276a1747a26f3979d5f8ea
Craig Deboer <craig@cdeboer.net>
2015-05-04 15:12:46 -0700

    Add pid file in tmp.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,6 +2,6 @@ root = "/home/craig/apps/hagglefree/current"
 listen "127.0.0.1:8080"
 worker_processes 2
 working_directory root
-pid "home/craig/apps/hagglefree/shared/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"

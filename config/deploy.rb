21519102b43b12c61a59d2135a7b59982f4a2092
Craig Deboer <craig@cdeboer.net>
2015-05-06 09:18:34 -0700

    Refactor Capistrano restart task code.

--- config/deploy.rb
+++ config/deploy.rb
@@ -39,23 +39,24 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      within '/home' do
+        execute :sudo, :service,'unicorn restart'
+      end
     end
   end
 
   after :publishing, :restart
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      within '/home' do
-        execute :sudo, :service,'unicorn restart'
-      end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     within '/home' do
+  #       execute :sudo, :service,'unicorn restart'
+  #     end
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-    end
-  end
+  #   end
+  # end
 
 end

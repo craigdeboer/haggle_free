d377706770e1698f266d7dba80124e3d14e4ee5a
Craig Deboer <craig@cdeboer.net>
2015-08-11 13:29:54 -0700

    Remove memoized instance variable from category listings controller

--- app/controllers/category_listings_controller.rb
+++ app/controllers/category_listings_controller.rb
@@ -4,14 +4,8 @@ class CategoryListingsController < ApplicationController
 	skip_before_action :require_login
 
 	def index
-		@listings = Listing.category_listings(category).page(params[:page]).per_page(10)
+    @category = Category.find(params[:category_id])
+		@listings = Listing.category_listings(@category).page(params[:page]).per_page(10)
 	end
-
-private
-
-	def category
-		@m_category ||= Category.find(params[:category_id])
-	end
-	helper_method :category
 
 end

22b2947af9262e53fe825fd187f692a72813f9ae
Craig Deboer <craig@cdeboer.net>
2015-08-11 13:44:06 -0700

    Refactor Expired Listings controller

--- app/controllers/expired_listings_controller.rb
+++ app/controllers/expired_listings_controller.rb
@@ -11,20 +11,23 @@ class ExpiredListingsController < ApplicationController
 		render 'index'
 	end
 
+  # This method will change the boolean "sold" attribute to true if the current user is the listing owner which should
+  # always be the case because the "mark as sold" button should only be displayed to the listing owner.
 	def mark_as_sold
 		@expired_listing = ExpiredListing.find(params[:id])
-		expired_listing_owner
-		@expired_listing.update_column(:sold, true)
-		redirect_to user_listings_path
+		if !expired_listing_owner?
+			flash[:notice] = "You may not edit another user's listing."
+			redirect_to root_path
+    else
+      @expired_listing.update_column(:sold, true)
+      redirect_to user_listings_path
+    end
 	end
 
 private
 
-	def expired_listing_owner
-		if @expired_listing.user != current_user
-			flash[:notice] = "You may not edit another user's listing."
-			redirect_to root_path
-		end
+	def expired_listing_owner?
+		@expired_listing.user == current_user
 	end
 
 end
